0c6cbd502898910725b643b19e4f4410f26bd15f
Yahir Moreno <yahirmb7568@gmail.com>
2022-10-03 20:10:30 -0500

    Added logic for buttons, results, questions...
    Finished the page app!
    Now i just need to add more pokemons to firebase.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,42 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { db } from './firebase_config';
 import { collection, getDocs } from 'firebase/firestore';
+import { motion } from 'framer-motion';
 
 function App() {
   const [PokemonObject, setPokemonObject] = useState([])
   const [Intro, setIntro] = useState(true)
+  const [Result, setResult] = useState(false)
   const [Amount, setAmount] = useState(0)
   const [Count, setCount] = useState(0)
+  const [Grade, setGrade] = useState(0)
+  const [Shortened, setShortened] = useState([])
+  const numbers = [1, 2, 5, 6, 8, 10, 15];
 
   const handleAnswer = (e) => {
-    if (e.target.value === PokemonObject[Count].data.Name) {
+    if (e.target.value === Shortened[Count].data.Name || e.target.value === Shortened[Count].data.Name.toLowerCase()) {
       setCount(Count => Count + 1)
-      e.target.value = ''
+      setGrade(Grade + 1)
+      e.target.value = '';
+      if (Shortened.length === Count + 1) {
+        setResult(true);
+      }
+    }
+  }
+
+  const Restart = () => {
+    setAmount(0);
+    setCount(0);
+    setGrade(0);
+    setIntro(true);
+    setResult(false);
+    setShortened([])
+  }
+
+  const handleNext = () => {
+    setCount(Count + 1);
+    if (Shortened.length === Count + 1) {
+      setResult(true);
     }
   }
 
@@ -37,7 +62,7 @@ function App() {
 
   const startGame = () => {
     shuffleArray(PokemonObject);
-    PokemonObject.length = Amount;
+    setShortened(PokemonObject.slice(0, Amount))
     setIntro(intro => !intro);
   }
 
@@ -48,20 +73,36 @@ function App() {
           {Intro
             ?
             <div className="app_intro">
-              <h1>Select amount of pokemon to guess!</h1>
-              <input type="number" onChange={(e) => setAmount(e.target.value)} />
-              <button onClick={startGame}>Start!</button>
+              <motion.h1 initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1 }}>Select amount of pokemon to guess!</motion.h1>
+              <div className="section">
+                {numbers.map(value => (
+                  <input type="button" key={value} value={value} onClick={v => setAmount(v.target.value)} />
+                ))}
+              </div>
+              <motion.button initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} onClick={startGame}>Start</motion.button>
             </div>
             :
-            <div className="app_questions">
-              <h1>Guess the pokemon!</h1>
-              <img src={PokemonObject[Count].data.Image} alt={PokemonObject[Count].data.Name} draggable="false" />
-              <input type="text" onChange={(e) => handleAnswer(e)} />
-            </div>
+            <>
+              {Result
+                ?
+                <div className="app_results">
+                  <h1>Congratulations, you've made it to the end!</h1>
+                  <p>This is your result: {Grade} of {Amount} </p>
+                  <button onClick={Restart}>Want to restart?</button>
+                </div>
+                :
+                <div className="app_questions">
+                  <motion.h1 initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1 }} >Who is that pokemon?</motion.h1>
+                  <motion.img initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: .5 }} src={Shortened[Count].data.Image} alt={Shortened[Count].data.Name} draggable="false" />
+                  <input type="text" onChange={(e) => handleAnswer(e)} />
+                  <button onClick={handleNext}>Next</button>
+                </div>
+              }
+            </>
           }
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
 
