48721c4ddabaf449e3436d456ee17f6694309c0e
Yahir Moreno <yahirmb7568@gmail.com>
2022-10-06 12:27:28 -0500

    Finished the project, made it responsive, added difficulty

--- src/App.js
+++ src/App.js
@@ -11,35 +11,9 @@ function App() {
   const [Amount, setAmount] = useState(0)
   const [Count, setCount] = useState(0)
   const [Grade, setGrade] = useState(0)
+  const [Difficulty, setDifficulty] = useState('Normal')
   const [Shortened, setShortened] = useState([])
   const numbers = [1, 2, 5, 6, 8, 10, 15];
-
-  const handleAnswer = (e) => {
-    if (e.target.value === Shortened[Count].data.Name || e.target.value === Shortened[Count].data.Name.toLowerCase()) {
-      setCount(Count => Count + 1)
-      setGrade(Grade + 1)
-      e.target.value = '';
-      if (Shortened.length === Count + 1) {
-        setResult(true);
-      }
-    }
-  }
-
-  const Restart = () => {
-    setAmount(0);
-    setCount(0);
-    setGrade(0);
-    setIntro(true);
-    setResult(false);
-    setShortened([])
-  }
-
-  const handleNext = () => {
-    setCount(Count + 1);
-    if (Shortened.length === Count + 1) {
-      setResult(true);
-    }
-  }
 
   useEffect(() => {
     const getData = async () => {
@@ -66,6 +40,33 @@ function App() {
     setIntro(intro => !intro);
   }
 
+  const handleAnswer = (e) => {
+    if (e.target.value === Shortened[Count].data.Name || e.target.value === Shortened[Count].data.Name.toLowerCase()) {
+      setCount(Count => Count + 1)
+      setGrade(Grade + 1)
+      e.target.value = '';
+      if (Shortened.length === Count + 1) {
+        setResult(true);
+      }
+    }
+  }
+
+  const handleNext = () => {
+    setCount(Count + 1);
+    if (Shortened.length === Count + 1) {
+      setResult(true);
+    }
+  }
+
+  const Restart = () => {
+    setAmount(0);
+    setCount(0);
+    setGrade(0);
+    setIntro(true);
+    setResult(false);
+    setShortened([])
+  }
+
   return (
     <div className="App">
       <div className="app_container">
@@ -73,15 +74,21 @@ function App() {
           {Intro
             ?
             <div className="app_intro">
-              <motion.h1 initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1 }}>Select amount of pokemon to guess!</motion.h1>
+              <motion.h1 initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1 }}>Select the amount of pokemon to guess!</motion.h1>
               <div className="section">
                 {numbers.map(value => (
                   <input type="button" key={value} value={value} onClick={v => setAmount(v.target.value)} />
                 ))}
               </div>
-              {/*               <div className="section">
-                <button></button>
-              </div> */}
+              <div className="section">
+                <button onClick={() => setDifficulty('Normal')}>Normal</button>
+                <button onClick={() => setDifficulty('Hard')}>Hard</button>
+                <button onClick={() => setDifficulty('Hell')}>Hell</button>
+              </div>
+              <div className="section2">
+                <p>Amount: <span>{Amount}</span></p>
+                <p>Difficulty: <span>{Difficulty}</span></p>
+              </div>
               <motion.button initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} onClick={startGame}>Start</motion.button>
             </div>
             :
@@ -94,12 +101,16 @@ function App() {
                   <button onClick={Restart}>Want to restart?</button>
                 </div>
                 :
-                <div className="app_questions">
+                <motion.div className="app_questions" initial={{ x: 1800 }} animate={{ x: 0 }} transition={{ duration: 1, type: 'spring' }}>
                   <motion.h1 initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1 }} >Who is that pokemon?</motion.h1>
-                  <motion.img initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: .5 }} src={Shortened[Count].data.Image} alt={Shortened[Count].data.Name} draggable="false" />
+                  {{
+                    'Normal': <img id="normal" src={Shortened[Count].data.Image} alt={Shortened[Count].data.Name} draggable="false" />,
+                    'Hard': <img id="hard" src={Shortened[Count].data.Image} alt={Shortened[Count].data.Name} draggable="false" />,
+                    'Hell': <motion.img id="hard" animate={{ rotate: [0, 180, 270, 180, 270, 90, 0, 300, 0] }} transition={{ repeat: Infinity, duration: 2 }} src={Shortened[Count].data.Image} alt={Shortened[Count].data.Name} draggable="false" />
+                  }[Difficulty]}
                   <input type="text" onChange={(e) => handleAnswer(e)} />
                   <button onClick={handleNext}>Next</button>
-                </div>
+                </motion.div>
               }
             </>
           }
